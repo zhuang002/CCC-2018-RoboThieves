b6c57fa890fb79b7b1c4faab922b9fbb6fdf8f0e
zhuang002 <zhuang001@gmail.com>
2018-11-28 17:12:58 -0500

    passed

--- src/robothieves/RoboThieves.java
+++ src/robothieves/RoboThieves.java
@@ -5,6 +5,8 @@
  */
 package robothieves;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 
 /**
@@ -16,31 +18,159 @@ public class RoboThieves {
     /**
      * @param args the command line arguments
      */
+    static char[][] map;
+    static int[][] route;
+    static int n,m;
+    static int step=0;
+    static ArrayList<Position> cameras=new ArrayList();
+    static ArrayList<Position> spaces=new ArrayList();
     public static void main(String[] args) {
         // TODO code application logic here
-        FactoryMap map=readInMap();
-        map.initialize();
-        FactoryPosition[] positions=new FactoryPosition[1];
-        positions[0]=map.getInitialPosition();
-        while (positions!=null && positions.length!=0) {
-            positions=map.walkOneStep(positions);
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        sc.nextLine();
+        map=new char[n][m];
+        route=new int[n][m];
+        Position start=null;
+        for (int i=0;i<n;i++) {
+            String row=sc.nextLine();
+            for (int j=0;j<m;j++) {
+                char c=row.charAt(j);
+                map[i][j]=c;
+                route[i][j]=-2;
+                if (c=='S') {
+                    start=new Position(i,j);
+                    route[i][j]=0;
+                } else if (c=='C') {
+                    route[i][j]=-1;
+                    cameras.add(new Position(i,j));
+                } else if (c=='W') {
+                    route[i][j]=-1;
+                } else if (c=='.'){
+                    spaces.add(new Position(i,j));
+                }
+            }
+        }  
+        ArrayList<Position> al=new ArrayList();
+        ArrayList<Position> nextAl=new ArrayList();
+        al.add(start);
+        
+        cameraCovers();
+        while (!al.isEmpty()) {
+            step++;
+            for (Position pos: al) {
+                nextAl.addAll(goNextStep(pos));
+            }
+            al=nextAl;
+            nextAl=new ArrayList();
+            
         }
-        map.printSteps();
+        
+        for (Position pos :spaces) {
+            System.out.println(route[pos.x][pos.y]>0?route[pos.x][pos.y]:-1);
+        }
     }
 
-    private static FactoryMap readInMap() {
-        Scanner sc=new Scanner(System.in);
-        int rows=sc.nextInt();
-        int cols=sc.nextInt();
-        sc.nextLine();
-        FactoryMap map=new FactoryMap(rows,cols);
-        for (int i=0;i<rows;i++) {
-            String line=sc.nextLine();
-            map.setRow(i,line);
-        }
-       
+    private static ArrayList<Position> goNextStep(Position pos) {
         
-        return map;
+        ArrayList<Position> ret=new ArrayList();
+        if (route[pos.x][pos.y]==-1) return ret;
+        moveTo(pos.x-1,pos.y,ret);
+        moveTo(pos.x+1,pos.y,ret);
+        moveTo(pos.x,pos.y-1,ret);
+        moveTo(pos.x,pos.y+1,ret);
+        return ret;
     }
-    
+
+    private static void moveTo(int x, int y, ArrayList<Position> ret) {
+        if (!isInBoundry(x,y)) return;
+        if (route[x][y]!=-2) return;
+        if (map[x][y]=='W' || map[x][y]=='C') {
+            return;
+        }
+        switch (map[x][y]) {
+            case 'L':
+                route[x][y]=-1;
+                moveTo(x,y-1,ret);
+                break;
+            case 'R':
+                route[x][y]=-1;
+                moveTo(x,y+1,ret);
+                break;
+            case 'U':
+                route[x][y]=-1;
+                moveTo(x-1,y,ret);
+                break;
+            case 'D':
+                route[x][y]=-1;
+                moveTo(x+1,y,ret);
+                break;
+            case '.':
+                route[x][y]=step;
+                ret.add(new Position(x,y));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static boolean isInBoundry(int x, int y) {
+        return x>0 && x<n && y>0 && y<m;
+    }
+
+    private static void cameraCovers() {
+        for (Position pos : cameras) {
+            int i=pos.x;
+            char c;
+            while (i>=0) {
+                i--;
+                c=map[i][pos.y];
+                if (c=='W') {
+                    break;
+                } else if (c=='.' || c=='S'){
+                    route[i][pos.y]=-1;
+                }
+            }
+            i=pos.x;
+            while (i<n) {
+                i++;
+                c=map[i][pos.y];
+                if (c=='W') {
+                    break;
+                } else if (c=='.' || c=='S'){
+                    route[i][pos.y]=-1;
+                }
+            }
+            i=pos.y;
+            while (i>=0) {
+                i--;
+                c=map[pos.x][i];
+                if (c=='W') {
+                    break;
+                } else if (c=='.'||c=='S'){
+                    route[pos.x][i]=-1;
+                }
+            }
+            i=pos.y;
+            while (i<m) {
+                i++;
+                c=map[pos.x][i];
+                if (c=='W') {
+                    break;
+                } else if (c=='.'||c=='S'){
+                    route[pos.x][i]=-1;
+                }
+            }
+        }
+    }
+
+    private static class Position {
+        int x;
+        int y;
+        public Position(int i,int j) {
+            this.x=i;
+            this.y=j;
+        }
+    }
 }
